36238387ec16518a1d4b88dab07eb60dcf2c5b4b
Michael Shannon <michaelshannon2003@gmail.com>
2018-08-14 22:27:25 +1000

    Add ability to specify the speed of the bullets

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -71,17 +71,18 @@ namespace Complete
             WeaponUsed.weapon = string.Format("Using {0}", weaponlist[currentWeapon].name);
             //ScoreBoard.weapon = weaponlist[currentWeapon].name;
             // Set the fired flag so only Fire is only called once.
-            m_Shell = weaponlist[currentWeapon].GetComponent<Rigidbody>();
-
+            var weapon = weaponlist[currentWeapon];
+            
+            
             // Create an instance of the shell and store a reference to it's rigidbody.
             Rigidbody shellInstance =
-                Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+                Instantiate(weapon.GetComponent<Rigidbody>(), m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
 
             // Set the shell's velocity to the launch force in the fire position's forward direction.
 
 
             // Set the shell's velocity to the launch force in the fire position's forward direction.
-            shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+            shellInstance.velocity = weapon.GetComponent<WeaponStats>().m_MinLaunchForce * m_FireTransform.forward;
         }
     }
 }
---
+++ Assets/Scripts/Shell/WeaponStats.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Complete
+{
+    public class WeaponStats : MonoBehaviour
+    {
+        public float m_MinLaunchForce = 30f;        // The force given to the shell if the fire button is not held.
+    }
+}
