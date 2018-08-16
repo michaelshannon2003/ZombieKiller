805d6309565733284b3ceab914fbba5240f6980d
Michael Shannon <michaelshannon2003@gmail.com>
2018-08-16 23:34:45 +1000

    Added abitity for ammo clips to run out

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -9,101 +8,108 @@ namespace Complete
     public class PlayerShooting : MonoBehaviour
     {
         #region EventPublisher
-      //  public delegate void DisplayWeaponSwitch(string message);
-      //  public static event DisplayWeaponSwitch OnWeaponEventMessage;
+        //  public delegate void DisplayWeaponSwitch(string message);
+        //  public static event DisplayWeaponSwitch OnWeaponEventMessage;
         #endregion EventPublisher
-
 
         public int m_PlayerNumber = 1;              // Used to identify the different players.
 
         public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
-        public float m_MinLaunchForce = 30f;        // The force given to the shell if the fire button is not held.
         private string m_FireButton;                // The input axis that is used for launching shells.
-        private float m_CurrentLaunchForce;         // The force that will be given to the shell when the fire button is released.        
-        private List<GameObject> weaponlist;
-        private int currentWeapon = 0;
-        private Rigidbody m_Shell;                   // Prefab of the shell.
 
-        private bool firing = false;
-      
+       private List<GameObject> weaponlist;
+        private int currentWeapon = 0;        
+
+        private bool able_to_fire = true;
+        private int remainingbullets;
+        private WeaponStats selectedWeaponStats;
+        private GameObject selectedWeapon;
 
         private void OnEnable()
-        {
-            m_CurrentLaunchForce = m_MinLaunchForce;
-         }
+        {          
+        }
 
         private void Awake()
         {
-            weaponlist = Resources.LoadAll<GameObject>("Prefabs/Weapons").ToList();
+            weaponlist = Resources.LoadAll<GameObject>("Prefabs/Weapons").ToList();            
         }
 
         private void Start()
         {
-           
             m_FireButton = "Fire" + m_PlayerNumber;
-           
+            SelectedWeaponStats();
         }
-
 
         private void Update()
         {
-           
-            // The fire axis is based on the player number.        
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 Switchweapons();
             }
-            if (Input.GetButtonDown(m_FireButton) && !firing)
-            {
-                firing = true;
-                 StartCoroutine(Fire()); 
-        
+            if (Input.GetButtonDown(m_FireButton) && able_to_fire){
+                StartCoroutine(FiringLoop());
             }
         }
 
+        private IEnumerator FiringLoop()
+        {
 
+            if (remainingbullets == 0)
+            {                
+                yield return StartCoroutine(Reload());                
+            }
+
+            if (able_to_fire)
+            {                
+                yield return StartCoroutine(ShootBullet());               
+            }           
+        }
+
+        private IEnumerator Reload()
+        {
+            able_to_fire = false;
+            EventManager.TriggerEvent("Message", "Reloading weapon");          
+            yield return new WaitForSeconds(selectedWeaponStats.reloadSpeed);
+            remainingbullets = selectedWeaponStats.ClipCapacity;
+            able_to_fire = true;
+        }
 
         private void Switchweapons()
         {
-           
+
             if (currentWeapon == (weaponlist.Count - 1))
             {
-                currentWeapon = 0;
+                currentWeapon =  0;
+                
             }
             else
             {
                 currentWeapon++;
             }
-
+            SelectedWeaponStats();
             EventManager.TriggerEvent("Message", "Switched to " + weaponlist[currentWeapon].name);
-            
         }
 
-
-        private IEnumerator Fire()
+        private void SelectedWeaponStats()
         {
-            EventManager.TriggerEvent("Message", "Firing Weapon");
-            //ScoreBoard.weapon = weaponlist[currentWeapon].name;
-            // Set the fired flag so only Fire is only called once.
-            var weapon = weaponlist[currentWeapon];
-            var weaponstats = weapon.GetComponent<WeaponStats>();
             
-            // Create an instance of the shell and store a reference to it's rigidbody.
-            ShootBullet(weapon.GetComponent<Rigidbody>(), weaponstats.m_MinLaunchForce);
-            yield return new WaitForSeconds(weaponstats.fireRate);
-            firing = false;          
+            selectedWeapon = weaponlist[currentWeapon];
+            selectedWeaponStats = selectedWeapon.GetComponent<WeaponStats>();
+            remainingbullets = selectedWeaponStats.ClipCapacity;
         }
-
-        private void ShootBullet(Rigidbody bullet, float launchForce)
+          
+        private IEnumerator ShootBullet()
         {
+            able_to_fire = false;
+
             Rigidbody shellInstance =
-                Instantiate(bullet, m_FireTransform.position, m_FireTransform.rotation);
+                Instantiate(selectedWeapon.GetComponent<Rigidbody>(), m_FireTransform.position, m_FireTransform.rotation);           
+            shellInstance.velocity = selectedWeaponStats.m_MinLaunchForce * m_FireTransform.forward;
 
-            // Set the shell's velocity to the launch force in the fire position's forward direction.
-
-
-            // Set the shell's velocity to the launch force in the fire position's forward direction.
-            shellInstance.velocity = launchForce * m_FireTransform.forward;
+            remainingbullets--;
+            EventManager.TriggerEvent("Message", string.Format("Firing Weapon. You have {0} bullets.", remainingbullets));
+            yield return new WaitForSeconds(selectedWeaponStats.fireRate);
+            able_to_fire = true;
         }
     }
 }
--- Assets/Scripts/Shell/WeaponStats.cs
+++ Assets/Scripts/Shell/WeaponStats.cs
@@ -6,5 +6,7 @@ namespace Complete
     {
         public float m_MinLaunchForce = 30f;        // The force given to the shell if the fire button is not held.
         public float fireRate;
+        public int ClipCapacity = 1;
+        public float reloadSpeed = 1;
     }
 }
