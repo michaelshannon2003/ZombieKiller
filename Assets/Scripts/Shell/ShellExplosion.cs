c805527c4b6ccbabc006c9af502cd2a9c5da57a8
Michael Shannon <michaelshannon2003@gmail.com>
2018-08-19 17:50:54 +1000

    Allowing the zombies to fire weapons

--- Assets/Scripts/Shell/ShellExplosion.cs
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -7,21 +7,23 @@ namespace Complete
         public float m_MaxLifeTime = 2f;                    // The time in seconds before the shell is removed.
         public float m_damagetaken = 50;
 
-        private void Start ()
+        private void Start()
         {
-            Destroy (gameObject, m_MaxLifeTime);
+            Destroy(gameObject, m_MaxLifeTime);
         }
 
 
-        private void OnTriggerEnter (Collider other)
+        private void OnTriggerEnter(Collider other)
         {
-            Debug.Log("Hitting for " + m_damagetaken + "damage");
-                if(other.CompareTag("Zombies")){
-                    other.GetComponentInParent<Health>().TakeDamage(m_damagetaken);
-                }
-           
+
+            if (!other.CompareTag(tag))
+            {
+                Debug.Log("Hitting for " + other.gameObject.name + " for damage");
+                other.GetComponentInParent<Health>().TakeDamage(m_damagetaken);
+            }
+
             Destroy(gameObject);
-                
+
         }
 
 
---
+++ Assets/Scripts/Zombie/Shooting.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    public Transform ammunition;
+    public float minimumDistanceFromPlayer = 15;
+    public float shotInterval = 0.5f;
+    private float shotTime = 0;
+    GameObject player;
+
+    private void Awake()
+    { 
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void Update()
+    {
+        var distance = Vector3.Distance(player.transform.position, transform.position);
+
+        if (distance >= minimumDistanceFromPlayer && (Time.time - shotTime) > shotInterval)
+        {
+            LookAtTarget();
+            Shoot();            
+        }
+    }
+
+    public void LookAtTarget()
+    {
+        transform.LookAt(player.transform);       
+    }
+
+
+    public void Shoot()
+    {
+        shotTime = Time.time;
+        var fireform = transform.Find("FireLocation").transform;
+        Rigidbody shellInstance =
+            Instantiate(ammunition.GetComponent<Rigidbody>(), fireform.position, fireform.rotation);
+        shellInstance.velocity = 10f * fireform.forward;
+    }
+}
