d61254b49bfc56dccc101f923a5828201c025b3a
Michael Shannon <michaelshannon2003@gmail.com>
2018-08-26 09:24:09 +1000

    Added  and particle effects to when zombies get hit

--- Assets/Scripts/Character/Health.cs
+++ Assets/Scripts/Character/Health.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Threading;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Complete
@@ -15,34 +17,55 @@ namespace Complete
         private Image m_FillImage;                           // The image component of the slider.
         public Color m_FullHealthColor = Color.green;       // The color the health bar will be when on full health.
         public Color m_ZeroHealthColor = Color.red;         // The color the health bar will be when on no health.
-      
-        public float m_StartingHealth = 100f;                      // How much health the tank currently has.
-        private float m_CurrentHealth;                      // How much health the tank currently has.
-        [HideInInspector] public bool m_Dead;
 
+        public float m_StartingHealth = 100f;                      // How much health the tank currently has.
+        private float m_CurrentHealth;
+        public ParticleSystem damageEffect;
+        public ParticleSystem deathEffect;
+        private AudioSource deathsound;
+
+        // Reference to the PlayerShooting script.
+        // How much health the tank currently has.
+        [HideInInspector] public bool m_Dead = false;
+
+
+        private void Awake()
+        {
+            SetAudio();
+        }
 
         private void OnEnable()
         {
             // When the tank is enabled, reset the tank's health and whether or not it's dead.
             m_CurrentHealth = m_StartingHealth;
-           
+
             // Update the health slider's value and color.
             SetHealthUI();
+            SetAudio();
         }
 
 
-        public void TakeDamage (float amount)
+        public void SetAudio()
+        {
+            deathsound = GetComponent<AudioSource>();
+        }
+
+        public void TakeDamage(float amount)
         {
             // Reduce current health by the amount of damage done.
             m_CurrentHealth -= amount;
 
             // Change the UI elements appropriately.
-            SetHealthUI ();
-
-            // If the current health is at or below zero and it has not yet been registered, call OnDeath.
-            if (m_CurrentHealth <= 0f)
+            SetHealthUI();
+            if (damageEffect != null)
             {
-                OnDeath ();
+                Destroy(Instantiate(damageEffect.gameObject, transform.position, transform.rotation) as GameObject, damageEffect.startLifetime);
+            }
+            // If the current health is at or below zero and it has not yet been registered, call OnDeath.
+            if (m_CurrentHealth <= 0f && !m_Dead)
+            {
+
+                OnDeath();
             }
         }
 
@@ -59,11 +82,19 @@ namespace Complete
         // Used during the phases of the game where the player should be able to control their tank.
         public void OnDeath()
         {
-
             m_Dead = true;
+
+            if (deathEffect != null)
+            {
+                Destroy(Instantiate(deathEffect.gameObject, transform.position, transform.rotation) as GameObject, deathEffect.startLifetime);
+
+            }
+
             gameObject.SetActive(false);
             EventManager.TriggerEvent("Message", "A death occured !!!");
 
         }
+
+
     }
 }
