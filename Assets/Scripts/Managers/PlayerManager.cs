7275a5f9d1628ac4613fff48cd2865945bdcbff4
Michael Shannon <michaelshannon2003@gmail.com>
2018-08-26 17:17:16 +1000

    Start to refactor movement scripts

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 namespace Complete
 {
     [Serializable]
+    [RequireComponent(typeof(MovementController))]
+    [RequireComponent(typeof(CharacterController))]
+
     public class PlayerManager : LivingEntity
     {
         public delegate void SendMessage(string message);
         public static event SendMessage OnWeaponSwitch;
 
+        public float moveSpeed = 5;
         public Color m_PlayerColor;                             // This is the color this tank will be tinted.
         public Transform m_SpawnPoint;                          // The position and direction the tank will have when it spawns.        
         [HideInInspector] public int m_PlayerNumber;            // This specifies which player this the manager for.        
@@ -17,29 +21,33 @@ namespace Complete
         private Weapons m_Weapons;
         private GameObject m_CanvasGameObject;                  // Used to disable the world space UI during the Starting and Ending phases of each round.
 
+        MovementController controller;
 
-        public void Start()
+        private void Awake()
+        {
+            controller = GetComponent<MovementController>();
+        }
+
+        public override void Start()
         {
 
             m_playerMovement = GetComponent<PlayerMovement>();
             m_Weapons = GetComponent<Weapons>();
-
-            m_playerMovement.m_PlayerNumber = m_PlayerNumber;
+          
             m_Weapons.m_PlayerNumber = m_PlayerNumber;
 
-            // Get all of the renderers of the tank.
-            MeshRenderer[] renderers = GetComponentsInChildren<MeshRenderer>();      
-            // Go through all the renderers...
-            for (int i = 0; i < renderers.Length; i++)
+        
+
+            foreach (MeshRenderer renderer in GetComponentsInChildren<MeshRenderer>())
             {
-                // ... set their material color to the color specific to this tank.
-                renderers[i].material.color = m_PlayerColor;
+
+                renderer.material.color = m_PlayerColor;
             }
 
             base.Start();
         }
 
-
+      
 
     }
 }
